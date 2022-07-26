a95d05121d304162268beb1e934e30f7eb548678
ezgiyilmaz <yilmaz15@itu.edu.tr>
2022-07-26 20:38:39 +0300

    frontend templates

---
+++ src/pinquiry-web-app/web-app-frontend/src/Pages/MonitorDetails.jsx
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import '../App.css';
+
+import { AppBar, Button, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Navbar from '../Components/Navbar';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+
+class Dashboard extends React.Component {
+    
+
+render(){
+  return (
+    <div style={{ width:"100%", height:"100%", margin:"0px", padding:"0px" }}>
+        <Navbar/>
+      <Grid
+          container
+          direction="row"
+          alignItems="stretch"
+        >
+          <Grid item 
+           container
+           direction="column"
+           justifyContent="center"
+           alignItems="flex-start"
+          lg={12} style={{margin:"0px", padding:"20px", background:"#D8E9A8", height:"100vh", color:"#1E5128"} } >
+            
+              
+            <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+      
+      <Grid spacing={0}>
+        <Grid item>
+          <Typography>
+            Text only
+          </Typography>
+          
+            <List>
+              
+                <ListItem>
+                  <ListItemText
+                    primary="Monitor #1"
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    primary="Monitor #2"
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    primary="Monitor #3"
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    primary="Monitor #4"
+                  />
+                </ListItem>
+              
+            </List>
+        </Grid>
+        
+      </Grid>
+    </Box>
+          </Grid>
+
+              
+          
+      </Grid>
+    </div>
+  );
+            }
+}
+
+export default Dashboard;
