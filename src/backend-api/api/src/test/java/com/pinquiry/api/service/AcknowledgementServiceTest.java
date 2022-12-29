8aa64d3ae0fa7dc32f9fcbdaa9bd4fc4ed586eba
yilmazezgi <yilmaz15@itu.edu.tr>
2022-12-29 17:47:36 +0300

    Unit Tests implemented with JUnit

---
+++ src/backend-api/api/src/test/java/com/pinquiry/api/service/AcknowledgementServiceTest.java
@@ -0,0 +1,21 @@
+package com.pinquiry.api.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AcknowledgementServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void checkIfNeededAcknowledgement() {
+    }
+}
